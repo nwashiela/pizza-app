0f395ccc533b2fcc0a7d626f4173af032713e99c
nwashiela <nwmahela12@gmail.com>
2022-10-04 10:33:06 +0200

    fixed packages

--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { ProductProvider } from "./context/ProductContext";
+import BrowserRouter from "react-router-dom/BrowserRouter";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-
-<ProductProvider>
-<App />
-</ProductProvider>
-
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <ProductProvider>
+      <App />
+    </ProductProvider>
+  </BrowserRouter>
 );
